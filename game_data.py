190dd3fd83e041005699290bfea72e87c2c3f556
TheYear2015 <TheYear2015@yeah.net>
2016-01-12 15:37:46 +0800

    ContentTree功能制作

--- game_data.py
+++ game_data.py
@@ -1,6 +1,6 @@
 import json
 
-class GameContentData:
+class ContentData:
 	def __init__(self):
 		self.baseId = 1
 		self.textSet = {}
@@ -21,7 +21,7 @@ class GameContentData:
 	def RemoveTextContent(self, id):
 		self.pop(id)
 		
-	def ClearTextContent(self):
+	def Clean(self):
 		self.textSet = {}
 		self.baseId = 1
 			
@@ -33,7 +33,7 @@ class GameContentData:
 			f.close()
 		
 	def ReadFromFile(self, fileName):
-		self.ClearTextContent()
+		self.Clean()
 		f = open(fileName, "r")
 		try:
 			d = f.read()
@@ -42,24 +42,96 @@ class GameContentData:
 			f.close()
 			keys = max(self.textSet.keys())
 			self.baseId = int(keys) + 1;
+
+
+def ContentTreeNode2Dict(obj):
+    #convert object to a dict
+    d = {}
+    d.update(obj.__dict__)
+    return d
+ 
+def Dict2ContentTreeNode(d):
+    #convert dict to object
+    if'__class__' in d:
+        args = dict((key.encode('ascii'), value) for key, value in d.items()) #get args
+        inst = ContentTreeNode(**args) #create new instance
+    else:
+        inst = d
+    return inst
 			
 class ContentTreeNode:
-	def __inti__(self, id):
+	def __init__(self, id, left='0', right='0', content='0'):
 		self.id = id
-		self.left = 0
-		self.right = 0
-		self.content = 0
+		self.left = left
+		self.right = right
+		self.content = content
+		
+	def __str__(self):
+		return "["+self.id + ","+self.left +","+self.right+","+self.content+"]";
+		
+	def Clean(self):
+		self.left = "0"
+		self.right = "0"
+		self.content = "0"
 			
 class ConentTree:
 	def __init__(self):
-		self.allNode = []
+		self.allNode = {"0":ContentTreeNode("0")}
+		self.baseId = 0
+		
+	def Clean(self):
+		self.allNode = {"0":ContentTreeNode("0")}
+		self.baseId = 0
+
+	def NewNode(self):
+		self.baseId = self.baseId + 1
+		n = ContentTreeNode(str(self.baseId))
+		self.allNode[n.id] = n
+		
+	def GetNode(self, id):
+		return self.allNode.get(id, None)
+
+	def GetSortedKeys(self):
+		return sorted(self.allNode)
+		
+	def SaveToFile(self, fileName):
+		f = open(fileName, "w")
+		try:
+			f.write(json.dumps(self.allNode, default=ContentTreeNode2Dict))
+		finally:
+			f.close()
+		
+	def ReadFromFile(self, fileName):
+		self.Clean()
+		f = open(fileName, "r")
+		try:
+			d = f.read()
+			self.allNode = json.loads(d,object_hook = Dict2ContentTreeNode)
+		finally:
+			f.close()
+			keys = max(self.allNode.keys())
+			self.baseId = int(keys) + 1;
+		
 			
 if __name__ == "__main__":
-	data = GameContentData()
+	data = ContentData()
 	print(data.GetTextContentById(str(0)))
 	for i in range(1, 1000):
 		data.InsertTextContent(str(i))
 	
 	data.SaveToFile("fffff")
 	data.ReadFromFile("fffff")
+	
+	tree = ConentTree();
+	n = tree.GetNode("0")
+	if n :
+		print(n)
+	else :
+		print("empyt")
+		
+	print(tree.GetSortedKeys())
+	for i in range(1, 1000):
+		tree.NewNode()
+	tree.SaveToFile("tttt")
+	tree.ReadFromFile("tttt")
 	
