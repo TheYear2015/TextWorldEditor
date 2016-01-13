6ce0287452c2747d2eacfff0a8217975eae4c823
TheYear2015 <TheYear2015@yeah.net>
2016-01-13 14:19:02 +0800

    界面滑动条添加

--- editor.py
+++ editor.py
@@ -16,6 +16,31 @@ class EditorMain:
 		
 		self.tool_bar.pack(side=tkinter.TOP, fill=tkinter.X)
 		
+		#view frame
+		frame = tkinter.Frame(self.root)
+		frame.pack(side=tkinter.TOP, expand=tkinter.YES, fill=tkinter.BOTH)
+		
+		self.view_frame = tkinter.Canvas(frame, scrollregion=(0,0,3000,3000))
+
+		scrl = tkinter.Scrollbar(frame, command=self.view_frame.yview)
+		self.view_frame.configure(yscrollcommand = scrl.set)
+		scrl.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+		
+		scrl = tkinter.Scrollbar(frame, orient = tkinter.HORIZONTAL, command=self.view_frame.xview)
+		self.view_frame.configure(xscrollcommand = scrl.set)
+		scrl.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+
+		self.view_frame.pack(side=tkinter.LEFT, expand=tkinter.YES, fill=tkinter.BOTH)
+		
+		
+		#test
+		self.TestNodeButton(10000)
+		
+	def TestNodeButton(self, count):
+		for i in range(1, count):
+			self.view_frame.create_window( 50 + 100 * (i % 1000), 30 + 40 * i / 1000, tags='BTN1',window=tkinter.Button(self.view_frame,text=u'侦听'+str(i),width=8))
+		
+		
 	def Save(self):
 		print("Save")
 
@@ -30,6 +55,8 @@ class EditorMain:
 		
 	def run(self):
 		self.root.mainloop()
+		
+		
 			
 if __name__ == "__main__":
 	editor = EditorMain()
